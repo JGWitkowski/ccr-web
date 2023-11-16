de40eb239c8dd5cfe72474051e5d59dc35f3f73a
Justin <jgwitkowski@gmail.com>
2023-11-16 15:42:52 -0500

    mock signin

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -81,49 +81,49 @@ exports.clam = (req, res) => {
 };
 
 exports.signin = (req, res) => {
-  User.findOne({
-    username: req.body.username,
-  })
-    .populate("roles", "-__v")
-    .exec((err, user) => {
-      if (err) {
-        res.status(500).send({ message: err });
-        return;
-      }
+  // User.findOne({
+  //   username: req.body.username,
+  // })
+  //   .populate("roles", "-__v")
+  //   .exec((err, user) => {
+  //     if (err) {
+  //       res.status(500).send({ message: err });
+  //       return;
+  //     }
 
-      if (!user) {
-        return res.status(404).send({ message: "User Not found." });
-      }
+  //     if (!user) {
+  //       return res.status(404).send({ message: "User Not found." });
+  //     }
 
-      var passwordIsValid = bcrypt.compareSync(
-        req.body.password,
-        user.password
-      );
+  //     var passwordIsValid = bcrypt.compareSync(
+  //       req.body.password,
+  //       user.password
+  //     );
 
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          accessToken: null,
-          message: "Invalid Password!",
-        });
-      }
+  //     if (!passwordIsValid) {
+  //       return res.status(401).send({
+  //         accessToken: null,
+  //         message: "Invalid Password!",
+  //       });
+  //     }
 
-      const token = jwt.sign({ id: user.id }, config.secret, {
-        algorithm: "HS256",
-        allowInsecureKeySizes: true,
-        expiresIn: 86400, // 24 hours
-      });
+  //     const token = jwt.sign({ id: user.id }, config.secret, {
+  //       algorithm: "HS256",
+  //       allowInsecureKeySizes: true,
+  //       expiresIn: 86400, // 24 hours
+  //     });
 
-      var authorities = [];
+  //     var authorities = [];
 
-      for (let i = 0; i < user.roles.length; i++) {
-        authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
-      }
-      res.status(200).send({
-        id: user._id,
-        user: user.username,
-        email: user.email,
-        roles: authorities,
-        token: token,
-      });
-    });
+  //     for (let i = 0; i < user.roles.length; i++) {
+  //       authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
+  //     }
+  res.status(200).send({
+    // id: user._id,
+    user: "Justin",
+    // email: user.email,
+    // roles: authorities,
+    token: "dumbtoken",
+  });
+  // });
 };
