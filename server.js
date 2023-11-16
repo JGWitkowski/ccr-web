13e7bab001d150aaa0cef37994e888483832a1a2
Justin <jgwitkowski@gmail.com>
2023-11-16 14:21:18 -0500

    serve from localhost

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, "localhost", () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
