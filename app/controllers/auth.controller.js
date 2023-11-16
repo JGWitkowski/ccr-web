6cfa95bade389f6bce83e6b488053651a98b4686
Justin <jgwitkowski@gmail.com>
2023-11-16 15:27:03 -0500

    fix error

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const config = require("../config/auth.config");
 const db = require("../models");
 const User = db.user;
 const Role = db.role;
+const Clam = db.role;
 
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
