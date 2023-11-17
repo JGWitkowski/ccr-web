f089a7cb8cbf54f53bdf8d1a648085b20a4e2fdd
Justin <jgwitkowski@gmail.com>
2023-11-17 14:32:15 -0500

    add new orgin

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dbConfig = require("./app/config/db.config");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://ccr-ui.web.app",
 };
 
 app.use(cors(corsOptions));
